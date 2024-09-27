c9fd472aee97fb27d6fc4f3a9aeecd3745279d56
mattchristenson <matt@rcc.team>
2024-09-27 13:21:03 -0500

    starting commit

---
+++ src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,54 @@
+package com.pluralsight;
+
+public class MathApp {
+
+    public static void main(String[] args) {
+// Question 1:
+// declare variables here
+
+
+
+// then code solution
+
+
+
+// then use System.out.println() to display results
+
+
+
+// ex: System.out.println("The answer is " + answer);
+// REPEAT FOR NEXT EXERCISE
+
+// QUESTION 2-----------------------
+
+        // declare variables here
+
+
+
+// then code solution
+
+
+
+// then use System.out.println() to display results
+
+// QUESTION 3--------------------------------------
+
+// declare variables here
+
+
+
+// then code solution
+
+
+
+// then use System.out.println() to display results
+
+
+
+
+
+
+
+    }
+
+}
